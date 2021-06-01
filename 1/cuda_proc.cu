3c614da44a748ffb827fe7ed891f52d613dadd40
nnzzll <nnzzll@zju.edu.cn>
2021-06-01 18:01:42 +0800

    查看cuda信息

---
+++ 1/cuda_proc.cu
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main()
+{
+    cudaDeviceProp prop;
+    int count;
+
+    cudaGetDeviceCount(&count);
+    std::cout<<"GPU num:"<<count<<std::endl;
+    cudaGetDeviceProperties(&prop,0);
+    std::cout<<"Max threads/block:"<<prop.maxThreadsPerBlock<<std::endl;
+    std::cout<<"Max block/SM:"<<prop.maxBlocksPerMultiProcessor<<std::endl;
+    std::cout<<"Max Grid size:"<<prop.maxGridSize[0]<<","<<prop.maxGridSize[1]<<","<<prop.maxGridSize[2]<<std::endl;
+}
